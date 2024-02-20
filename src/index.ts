a4203ac3f516c44862c03e53f054c8b0fca3395c
Shark Code <rajanand18092002@gmail.com>
2024-02-20 19:24:33 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import express from "express";
+const { S3Client, PutObjectCommand, GetObjectCommand } = require('@aws-sdk/client-s3');
+
+const s3 = new S3Client({
+    region: 'ap-south-1',
+  credentials: {
+    //@ts-ignore
+    accessKeyId: 'AKIAQ5ZCGUVMDLK3PGF3',
+    //@ts-ignore
+    secretAccessKey: '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',
+  },
+})
+
+const app = express();
+//@ts-ignore
+app.get("/*", async (req, res) => {
+    // id.100xdevs.com
+    const host = req.hostname;
+
+    const id = host.split(".")[0];
+    const filePath = req.path;
+    console.log(`dist/${id}${filePath}`);
+    //dist/sdyw5/index.html
+    //dist/sdyw5/index.html
+    // console.log(res);
+    const contents = await s3.send(new GetObjectCommand({
+        Bucket: "deployitnow",
+        Key: `dist/${id}${filePath}`
+    }));
+    // console.log(contents.Body);
+    const type = filePath.endsWith("html") ? "text/html" : filePath.endsWith("css") ? "text/css" : "application/javascript"
+    console.log("non",type);
+    res.setHeader("Content-Type", type);
+    
+    res.send(contents.Body.toString());
+})
+
+app.listen(3001);
